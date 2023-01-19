8933f99ff7ac74c58cc91003d1c235e7ad8cfd32
doinkythederp <doinkythederp@icloud.com>
2023-01-18 22:56:54 -0800

    feat: add function for getting mappings

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+include!(concat!(env!("OUT_DIR"), "/mappings.rs"));
+
+/// Finds the human-readable name of a server from its address.
+pub fn get_pretty_name(address: &str) -> Option<&str> {
+    SERVER_MAPPINGS.get(address).copied()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::get_pretty_name;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn hypixel() {
+        let result = get_pretty_name("mc.hypixel.net");
+        assert_eq!(result, Some("Hypixel"));
+    }
+
+    #[test]
+    fn mineplex() {
+        let result = get_pretty_name("mineplex.com");
+        assert_eq!(result, Some("Mineplex"));
     }
 }
